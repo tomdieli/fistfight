862c17ebd9814217c1244e7040ca8750d0d3dc5c
Tom Dieli <tomdieli@hotmail.com>
2022-05-11 17:57:36 -0700

    bla

--- Config.py
+++ Config.py
@@ -4,7 +4,8 @@ from distutils.debug import DEBUG
 
 class Config(object):
     DATABASE_URL = environ.get('DATABASE_URL')
-    REDIS_URL = environ.get('REDIS_TLS_URL')
+    # REDIS_URL = environ.get('REDIS_TLS_URL')
+    REDIS_URL = environ.get('REDIS_URL')
     SECRET_KEY = urandom(12).hex()
     
 
