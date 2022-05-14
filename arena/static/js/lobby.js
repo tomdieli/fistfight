018c019374a2bb07aadd5f27c1e04d0f89d380e0
Tom Dieli <tomdieli@hotmail.com>
2022-05-13 19:40:06 -0700

    bla

--- arena/static/js/lobby.js
+++ arena/static/js/lobby.js
@@ -2,11 +2,11 @@ var thisUser = JSON.parse(thisUser);
 var games = JSON.parse(games);
 var users = JSON.parse(users);
 
-var url = new URL('wss://' + document.domain + '/lobby');
+//url = new URL('https://' + document.domain + '/lobby');
 
 socket = io.connect(
-  url,
-  cors_allowed_origins=url 
+  'https://' + document.domain + '/lobby',
+  //cors_allowed_origins=url 
 );
 
 document.querySelector("#new_game").addEventListener("click", function(event) {
